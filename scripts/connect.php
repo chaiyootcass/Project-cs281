5c101a602619955ab0a13525338d6919fb0a0b48
chaiyootcass <peerapong_7@outlook.com>
2018-04-20 17:55:55 +0700

    Add files via upload

---
+++ scripts/connect.php
@@ -0,0 +1,7 @@
+<?php
+$db_host = "localhost";
+$db_username = "root";
+$db_pass = "";
+$db_name = "cd";
+$con = mysqli_connect("$db_host", "$db_username", "$db_pass") or die("could not connect to mysql");
+mysqli_select_db($con, "$db_name") or die("no database");
